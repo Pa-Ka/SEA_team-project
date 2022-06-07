f1d50597c0dc959d003de5bba37b880cbf66496a
sjmjys954646 <sjmjys954646@gmail.com>
2022-06-07 22:53:21 +0900

    feat : login FIN

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, jsonify, url_for
+from flask_cors import CORS, cross_origin
+import requests
 
 app = Flask(__name__, static_url_path='/static')
+
+CORS(app, supports_credentials=True)
+
 
 @app.route('/')
 def login():
@@ -9,6 +14,34 @@ def login():
 @app.route('/index')
 def index():
     return  render_template('index.html')
+
+@app.route("/callback")
+def CallBack():
+    params = request.args.to_dict()
+    code = params.get("code")
+
+    client_id = "xneNfIal5CkgtXiMRHOo"
+    client_secret = "8v1fnuPn29"
+
+    token_request = requests.get(
+        f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&code={code}")
+    token_json = token_request.json()
+    print(token_json)
+    access_token = token_json.get("access_token")
+    profile_request = requests.get("https://openapi.naver.com/v1/nid/me",
+                                   headers={"Authorization": f"Bearer {access_token}"}, )
+    profile_data = profile_request.json()
+    print(profile_data)
+    return redirect(url_for('index', profile_data = profile_data))
+
+
+@app.route("/naver")
+def NaverLogin():
+    client_id = "xneNfIal5CkgtXiMRHOo"
+    redirect_uri = "http://localhost:80/callback"
+    url = f"https://nid.naver.com/oauth2.0/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    return redirect(url)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- static/assets/js/naverLogin.js
+++ static/assets/js/naverLogin.js
@@ -1,29 +1,4 @@
-const naverLogin = new naver.LoginWithNaverId(
-    {
-        clientId: "xneNfIal5CkgtXiMRHOo",
-        callbackUrl: "http://localhost:80/index",
-        callbackHandle: true,
-        loginButton: {color: "green", type: 2, height: 40}
-    }
-);
 
-naverLogin.init();
-
-
-window.addEventListener('load', function () {
-    naverLogin.getLoginStatus(function (status) {
-        if (status) {
-            var email = naverLogin.user.getEmail(); // 필수로 설정할것을 받아와 아래처럼 조건문을 줍니다.
-
-            console.log(naverLogin.user);
-
-            if( email == undefined || email == null) {
-                alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
-                naverLogin.reprompt();
-                return;
-            }
-        } else {
-            console.log("callback 처리에 실패하였습니다.");
-        }
-    });
-});
+function login() {
+ location.href = "http://localhost:80/naver";
+}
