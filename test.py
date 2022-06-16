029e0ca4f2b2d85a2b9b69caad7ba0368c87774d
junssg <80884697+junssg@users.noreply.github.com>
2022-06-17 00:37:32 +0900

    DB connect

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ app = Flask(__name__, static_url_path='/static')
 mysql=MySQL()
 
 app.config['MYSQL_DATABASE_USER']='local'
-app.config['MY_SQL_DATABASE_PASSWORD']='roqkf2xla'
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_PORT']=3306
+app.config['MYSQL_DATABASE_PASSWORD']='roqkf2xla'
+app.config['MYSQL_DATABASE_HOST']='plan.is119.kr'
+app.config['MYSQL_DATABASE_PORT']=3308
 app.config['MYSQL_DATABASE_DB']='termDB'
 
 mysql.init_app(app)
@@ -22,17 +22,12 @@ def index():
 @app.route('/ranking',methods=['GET'])
 def ranking():
     if request.method == "GET":
-        nickname=request.form['nickname']
-        current_exp=request.form['current_exp']
         conn = mysql.connect()
         cursor = conn.cursor()
-        sql = "SELECT nickname, current_exp FROM example"
+        sql = "SELECT nickname,current_exp FROM user ORDER BY current_exp DESC LIMIT 5"
         cursor.execute(sql)
         data = cursor.fetchall()
-        for i in range(len(data)):
-            print(data)
-        return render_template('ranking.html',data=data)
-
+        return render_template('ranking.html',data = data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='80')
