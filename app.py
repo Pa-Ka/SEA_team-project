ce76d433b665366e87b2522eb6268a663d75e44c
sjmjys954646 <sjmjys954646@gmail.com>
2022-06-18 20:45:06 +0900

    naverLogin Database

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, redirect, request, jsonify, url_for
+from flask import Flask, render_template, redirect, request, jsonify, url_for, g
 from flask_cors import CORS, cross_origin
 import requests
+from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__, static_url_path='/static')
 
 CORS(app, supports_credentials=True)
 
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'local'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'roqkf2xla'
+app.config['MYSQL_DATABASE_HOST'] = 'plan.is119.kr'
+app.config['MYSQL_DATABASE_PORT'] = 3308
+app.config['MYSQL_DATABASE_DB'] = 'termDB'
+
+mysql.init_app(app)
 
 @app.route('/')
 def login():
@@ -13,7 +24,7 @@ def login():
 
 @app.route('/index')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/callback")
 def CallBack():
@@ -31,9 +42,39 @@ def CallBack():
     profile_request = requests.get("https://openapi.naver.com/v1/nid/me",
                                    headers={"Authorization": f"Bearer {access_token}"}, )
     profile_data = profile_request.json()
-    print(profile_data)
-    return redirect(url_for('index', profile_data = profile_data))
 
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    uid = profile_data['response']['id']
+    sql = "SELECT * FROM user WHERE uid = '%s'" % (uid)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    userData = {}
+    #유저가 없을때
+    #유저 db에 생성 후 INSERT
+    #유저 데이터 가져와서 출석체크 시키기
+    if data == ():
+        uid = profile_data['response']['id']
+        current_exp = 0
+        attendance = 1
+        nickname = profile_data['response']['email'].split('@')[0]
+        recent = str(datetime.today())[0:10]
+        permission = 1
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sql = "INSERT INTO user(uid, current_exp, attendance, nickname, recent, permission) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (uid, current_exp, attendance, nickname, recent, permission))
+        conn.commit()
+    else :
+        dataName = ["uid", "current_exp", "attendance", "nickname", "recent", "permission"]
+        for index in range(0,len(data[0])):
+            if index == 4 :
+                userData[dataName[index]] = str(data[0][index])
+            else :
+                userData[dataName[index]] = data[0][index]
+        profile_data['userData'] = userData
+
+    return redirect(url_for('index', profile_data = profile_data))
 
 @app.route("/naver")
 def NaverLogin():
--- static/assets/js/naverLoginCallback.js
+++ static/assets/js/naverLoginCallback.js
@@ -3,9 +3,10 @@ function setLoginStatus(){
 	    location.replace("http://127.0.0.1:80/");
 }
 
-
 var str = JSON.parse(decodeURIComponent(window.location.search.replace(/\+/g,'')).replace(/\'/g,'"').slice(14));
 console.log(str)
 
-document.getElementById("naverIdLogin_nickname").innerHTML = str.response.nickname + '님';
+var nickname  = str.response.email.split('@');
+nickname = nickname[0];
 
+document.getElementById("naverIdLogin_nickname").innerHTML = nickname + '님';
